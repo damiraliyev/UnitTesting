212d1914b42cb6c9fba7dac33f635d48d7a42f3a
Damir Aliyev <damiraliyevcs@gmail.com>
2023-10-02 10:31:28 +0600

    Add PhotoApp project.

---
+++ PhotoApp/PhotoApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  PhotoApp
+//
+//  Created by Damir Aliyev on 02.10.2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+

cbbc562ae21a5915bc3abf3610bf09db2f409549
Damir Aliyev <damiraliyevcs@gmail.com>
2023-10-02 10:40:25 +0600

    Add "Tests" suffix to the test class name.

---
+++ PhotoApp/PhotoAppTests/SignupFormModelValidatorTests.swift
@@ -0,0 +1,24 @@
+//
+//  SignupFormModelValidator.swift
+//  PhotoAppTests
+//
+//  Created by Damir Aliyev on 02.10.2023.
+//
+
+import XCTest
+
+final class SignupFormModelValidatorTests: XCTestCase {
+
+    override func setUpWithError() throws {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDownWithError() throws {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+    
+    func testSignupFormModelValidator_WhenValidFirstNameProvided_ShouldReturnTrue() {
+        
+    }
+
+}
